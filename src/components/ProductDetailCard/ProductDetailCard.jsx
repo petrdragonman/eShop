11d198382189abb8635ed6c3866290e7c2e1b1d8
petrdragonman <petrdragonman@me.com>
2025-01-24 19:11:13 +1100

    adds home page

---
+++ src/components/ProductDetailCard/ProductDetailCard.jsx
@@ -0,0 +1,19 @@
+import classes from "./ProductDetailCard.module.scss";
+
+const ProductDetailCard = () => {
+  <article className={classes.card}>
+    <img src="./paddle.jpg" alt="wooden paddle" className={classes.img} />
+    <section className={classes.description}>
+      <p className={classes.title}>
+        Z&J SPORT Adjustable Dragon Boat Paddle, IDBF Approved Boat Oars, Carbon
+        Fiber Paddle Adjustable T Handle with Adjuster for Dragon Boat Paddler
+        (Range: 43”-52” /110cm-132cm)
+      </p>
+      <p className={classes.price}>$105.00</p>
+      <p className={classes.shipping}>shiping calculated at checkout</p>
+    </section>
+    <button className={classes.btn}>view more</button>
+  </article>;
+};
+
+export default ProductDetailCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,20 @@
+import ProductCard from "../../components/ProductCard/ProductCard";
+import classes from "./Home.module.scss";
+
 const Home = () => {
-    return (
-        <h2>HOME</h2>
-    );
+  return (
+    <>
+      <h3>Dragon Boat Carbon Paddles</h3>
+      <section className={classes.wrapper}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </section>
+    </>
+  );
 };
 
 export default Home;
